cb3db5a7465fe17cfeb16c43aa09015433c828e1
Zeizvant <oxanashvilizezva@gmail.com>
2024-01-26 16:20:20 +0400

    add info section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { Info } from './components/Info/Info';
 import { NavBar } from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Info />
     </div>
   );
 }
---
+++ src/components/Info/Info.jsx
@@ -0,0 +1,22 @@
+import './index.css'
+
+export const Info  = () => {
+    return (
+        <div className='info'>
+            <div className='cover-container'>
+                <h1><span className='main-text'>TBC x USAID</span><br /> ᲢᲔᲥᲜᲝᲚᲝᲒᲘᲣᲠᲘ ᲒᲐᲜᲐᲗᲚᲔᲑᲘᲡᲗᲕᲘᲡ</h1>
+            </div>
+            <div className='info-section'>
+                <p className='info-paragraph'>
+                    „ TBC x USAID - ტექნოლოგიური განათლებისთვის “ პროგრამა  
+                    საინფორმაციო ტექნოლოგიებით დაინტერესებულ ადამიანებს 
+                    გთავაზობთ სრულად დაფინანსებულ ონლაინ საგანმანათლებლო 
+                    პრაქტიკულ კურსებს  სხვადასხვა ICT მიმართულებით. 
+                    წარმატებულ კურსდამთავრებულებს ეძლევათ შესაძლებლობა 
+                    დასაქმდნენ თიბისისა და მის პარტნიორ კომპანიებში.
+                </p>
+                <div class='main-button'>გაიგე მეტი</div>
+            </div>
+        </div>
+    )
+}
